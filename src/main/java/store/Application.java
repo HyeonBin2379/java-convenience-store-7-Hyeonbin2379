1c7c8ac3fb355dcc429790e279398579cc8d926c
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-11-07 17:41:04 +0900

    feat(Application) : 편의점 구매 기능 일부 구현 - 구매 이전 재고 현황 출력 기능 구현 - 구매할 상품명, 구매 수량 입력 기능 구현 - 일반 구매 결과에 관한 영수증 출력 기능 구현 - 구매 재시도 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,37 @@
 package store;
 
+import static store.config.AppConfig.inventoryDao;
+import static store.config.AppConfig.outputView;
+import static store.config.AppConfig.retryController;
+import static store.util.message.InputMessage.MEMBERSHIP_DISCOUNT;
+import static store.util.message.InputMessage.PROMOTION_AVAILABLE;
+import static store.util.message.InputMessage.RETRY_PURCHASE;
+
+import java.io.IOException;
+import store.model.Promotion;
+import store.model.Purchase;
+import store.model.Purchases;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    public static void main(String[] args) throws IOException {
+        do {
+            outputView.displayInventories(inventoryDao.getAll());
+            Purchases purchases = retryController.retryPurchaseItemInput();
+            for (Purchase purchase : purchases.getAll()) {
+
+            }
+            if (retryController.retryOptionInput(MEMBERSHIP_DISCOUNT)) {
+                purchases.setMembershipDiscount();
+            }
+            outputView.displayReceipt(purchases);
+        } while(retryController.retryOptionInput(RETRY_PURCHASE));
+    }
+
+    private static void discountByPromotion(Purchase purchase, Promotion promotion) {
+
+    }
+    private static String makeString(String name, int giveawayCount) {
+        return String.format(PROMOTION_AVAILABLE, name, giveawayCount);
     }
 }
