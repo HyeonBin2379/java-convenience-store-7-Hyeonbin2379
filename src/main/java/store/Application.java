59df93ce3b631a5cf5eb481595b8505010561194
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-11-08 09:50:07 +0900

    refactor(Application) : 코드 가독성 개선

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,37 +1,14 @@
 package store;
 
-import static store.config.AppConfig.inventoryDao;
-import static store.config.AppConfig.outputView;
-import static store.config.AppConfig.retryController;
-import static store.util.message.InputMessage.MEMBERSHIP_DISCOUNT;
-import static store.util.message.InputMessage.PROMOTION_AVAILABLE;
-import static store.util.message.InputMessage.RETRY_PURCHASE;
-
 import java.io.IOException;
-import store.model.Promotion;
-import store.model.Purchase;
-import store.model.Purchases;
+import store.config.PurchaseConfig;
+import store.controller.PurchaseController;
 
 public class Application {
 
+    private static final PurchaseController PURCHASE_CONTROLLER = PurchaseConfig.getPurchaseController();
+
     public static void main(String[] args) throws IOException {
-        do {
-            outputView.displayInventories(inventoryDao.getAll());
-            Purchases purchases = retryController.retryPurchaseItemInput();
-            for (Purchase purchase : purchases.getAll()) {
-
-            }
-            if (retryController.retryOptionInput(MEMBERSHIP_DISCOUNT)) {
-                purchases.setMembershipDiscount();
-            }
-            outputView.displayReceipt(purchases);
-        } while(retryController.retryOptionInput(RETRY_PURCHASE));
-    }
-
-    private static void discountByPromotion(Purchase purchase, Promotion promotion) {
-
-    }
-    private static String makeString(String name, int giveawayCount) {
-        return String.format(PROMOTION_AVAILABLE, name, giveawayCount);
+        PURCHASE_CONTROLLER.start();
     }
 }
