16eeb641ad4f1a3f28c6631b90476dddc4ca411a
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-11-08 16:27:23 +0900

    fix(OutputMessage) : 출력 메시지 형식 수정 - 콘솔창 기준으로 좌우 정렬이 가능하도록 메시지 출력 형식 수정

--- src/main/java/store/util/message/OutputMessage.java
+++ src/main/java/store/util/message/OutputMessage.java
@@ -3,18 +3,18 @@ package store.util.message;
 public final class OutputMessage {
 
     public static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n";
-    public static final String OUTPUT_ITEM_INFO = "- %s %,d원 %s %s\n";
-    public static final String OUTPUT_ITEM_COUNT = "%,d개";
+    public static final String ITEM_INFO = "- %s %,d원 %s %s\n";
+    public static final String ITEM_COUNT = "%,d개";
 
-    public static final String OUTPUT_RECEIPT_TITLE = "\n===========W 편의점=============\n상품명\t\t수량\t금액";
-    public static final String OUTPUT_PURCHASED_ITEM = "%s\t\t%,d \t%,d\n";
+    public static final String RECEIPT_TITLE = "\n===========W 편의점=============\n%-12s\t%-6s\t%-5s\n";
+    public static final String PURCHASED_ITEM = "%-12s\t%,-6d\t%,-1d\n";
 
-    public static final String OUTPUT_GIVEAWAY_TITLE = "===========증\t정=============";
-    public static final String OUTPUT_GIVEAWAY_ITEM = "%s\t\t%,d\n";
+    public static final String GIVEAWAY_TITLE = "===========증\t정=============";
+    public static final String GIVEAWAY_ITEM = "%-12s\t%,-1d\n";
 
-    public static final String OUTPUT_PURCHASE_RESULT_TITLE = "==============================";
-    public static final String OUTPUT_TOTAL_COST = "총구매액\t\t%,d\t%,d\n";
-    public static final String OUTPUT_TOTAL_PROMOTION_DISCOUNT = "행사할인\t\t\t-%,d\n";
-    public static final String OUTPUT_TOTAL_MEMBERSHIP_DISCOUNT = "멤버십할인\t\t\t-%,d\n";
-    public static final String OUTPUT_FINAL_COST = "내실돈\t\t\t %,d\n";
+    public static final String PURCHASE_RESULT_TITLE = "==============================";
+    public static final String OUTPUT_TOTAL_COST = "%-12s\t%,-6d\t%,-5d\n";
+    public static final String OUTPUT_TOTAL_PROMOTION_DISCOUNT = "%-18s\t-%,-5d\n";
+    public static final String OUTPUT_TOTAL_MEMBERSHIP_DISCOUNT = "%-18s\t-%,-5d\n";
+    public static final String OUTPUT_FINAL_COST = "%-23s%,5d\n";
 }
