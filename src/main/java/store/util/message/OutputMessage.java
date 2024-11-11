5196122b373fece637be9aa92fb5165c799b02c7
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-11-11 17:16:04 +0900

    refactor(OutputMessage) : 구매 결과 출력 시 좌우 정렬 형식 변경

--- src/main/java/store/util/message/OutputMessage.java
+++ src/main/java/store/util/message/OutputMessage.java
@@ -6,15 +6,15 @@ public final class OutputMessage {
     public static final String ITEM_INFO = "- %s %,d원 %s %s\n";
     public static final String ITEM_COUNT = "%,d개";
 
-    public static final String RECEIPT_TITLE = "\n===========W 편의점=============\n%-12s\t%-6s\t%-5s\n";
-    public static final String PURCHASED_ITEM = "%-12s\t%,-6d\t%,-1d\n";
+    public static final String RECEIPT_TITLE = "\n==============W 편의점================\n%-16s\t%-8s%s\n";
+    public static final String PURCHASED_ITEM = "%-16s\t%,-9d %,-7d\n";
 
-    public static final String GIVEAWAY_TITLE = "===========증\t정=============";
-    public static final String GIVEAWAY_ITEM = "%-12s\t%,-6d\n";
+    public static final String GIVEAWAY_TITLE = "=============증     정===============";
+    public static final String GIVEAWAY_ITEM = "%-16s\t%,-16d\n";
 
-    public static final String PURCHASE_RESULT_TITLE = "==============================";
-    public static final String OUTPUT_TOTAL_COST = "%-12s\t%,-6d\t%,-5d\n";
-    public static final String OUTPUT_TOTAL_PROMOTION_DISCOUNT = "%-18s\t-%,-5d\n";
-    public static final String OUTPUT_TOTAL_MEMBERSHIP_DISCOUNT = "%-18s\t-%,-5d\n";
-    public static final String OUTPUT_FINAL_COST = "%-23s%,5d\n";
+    public static final String PURCHASE_RESULT_TITLE = "====================================";
+    public static final String OUTPUT_TOTAL_COST = "%-16s\t%,-9d %,-7d\n";
+    public static final String OUTPUT_TOTAL_PROMOTION_DISCOUNT = "%-24s\t  -%,-4d\n";
+    public static final String OUTPUT_TOTAL_MEMBERSHIP_DISCOUNT = "%-24s\t  -%,-4d\n";
+    public static final String OUTPUT_FINAL_COST = "%-24s\t  %,6d\n";
 }
